40111106fd7f28ef3ceace52c5e96a187c5a7768
sebastian.vasquez <sebastianvasquez@prap001873.grupo-rapp.ic>
2017-12-18 16:23:51 -0500

    Funciones de agregacion y borrado de ahorro

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,20 +1,70 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+const firestore = admin.firestore();
+//Get
 exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
 });
 exports.createUser = functions.firestore
     .document('users/{userId}')
     .onCreate(event => {
-    // Retrieve the current and previous value
-    const data = event.data.data();
     // Then return a promise of a set operation to update the count
     return event.data.ref.set({
         prueba: "Success!!!"
     }, { merge: true });
+});
+exports.removeUserFromSaving = functions.firestore
+    .document('savings/{savingId}/users/{userId}')
+    .onDelete(event => {
+    const savingRef = firestore.doc(`savings/${event.params.savingId}`);
+    const userRef = firestore.doc(`users/${event.params.userId}`);
+    // const setUserSaving = firestore.doc(`users/${event.params.userId}`).set(data, { merge: true })
+    const setUserSaving = firestore.runTransaction(t => {
+        return t.get(userRef)
+            .then(doc => {
+            const savingsArr = doc.data().savings;
+            if (!savingsArr)
+                return;
+            savingsArr.splice(savingsArr.indexOf(event.params.savingId));
+            t.set(userRef, { savings: savingsArr }, { merge: true });
+        });
+    });
+    const setCount = firestore.runTransaction(t => {
+        return t.get(savingRef)
+            .then(doc => {
+            const newCount = doc.data().userCount - 1;
+            t.update(savingRef, { userCount: newCount });
+        });
+    });
+    return Promise.all([setUserSaving, setCount]);
+});
+exports.addUserToSaving = functions.firestore
+    .document('savings/{savingId}/users/{userId}')
+    .onCreate(event => {
+    const savingRef = firestore.doc(`savings/${event.params.savingId}`);
+    const userRef = firestore.doc(`users/${event.params.userId}`);
+    // const setUserSaving = firestore.doc(`users/${event.params.userId}`).set(data, { merge: true })
+    const setUserSaving = firestore.runTransaction(t => {
+        return t.get(userRef)
+            .then(doc => {
+            let savingsArr = doc.data().savings;
+            if (!savingsArr) {
+                savingsArr = [];
+            }
+            savingsArr.push(event.params.savingId);
+            t.set(userRef, { savings: savingsArr }, { merge: true });
+        });
+    });
+    const setCount = firestore.runTransaction(t => {
+        return t.get(savingRef)
+            .then(doc => {
+            const newCount = doc.data().userCount + 1;
+            t.update(savingRef, { userCount: newCount });
+        });
+    });
+    return Promise.all([setUserSaving, setCount]);
 });
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,11 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
+admin.initializeApp(functions.config().firebase);
+
+const firestore = admin.firestore();
+
+//Get
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
@@ -10,11 +13,60 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 exports.createUser = functions.firestore
   .document('users/{userId}')
   .onCreate(event => {
-    // Retrieve the current and previous value
-    const data = event.data.data();
-
     // Then return a promise of a set operation to update the count
     return event.data.ref.set({
       prueba: "Success!!!"
-    }, {merge: true});
+    }, { merge: true });
+  });
+
+exports.removeUserFromSaving = functions.firestore
+  .document('savings/{savingId}/users/{userId}')
+  .onDelete(event => {
+    const savingRef = firestore.doc(`savings/${event.params.savingId}`);
+    const userRef = firestore.doc(`users/${event.params.userId}`);
+    // const setUserSaving = firestore.doc(`users/${event.params.userId}`).set(data, { merge: true })
+    const setUserSaving = firestore.runTransaction(t => {
+      return t.get(userRef)
+        .then(doc => {
+          const savingsArr = doc.data().savings;
+          if(!savingsArr) return;
+          savingsArr.splice(savingsArr.indexOf(event.params.savingId));
+          t.set(userRef, { savings: savingsArr }, { merge: true });
+        })
+    })
+    const setCount = firestore.runTransaction(t => {
+      return t.get(savingRef)
+        .then(doc => {
+          const newCount = doc.data().userCount - 1;
+          t.update(savingRef, { userCount: newCount });
+        })
+    })
+    return Promise.all([setUserSaving, setCount]);
+  });
+
+exports.addUserToSaving = functions.firestore
+  .document('savings/{savingId}/users/{userId}')
+  .onCreate(event => {
+    const savingRef = firestore.doc(`savings/${event.params.savingId}`);
+    const userRef = firestore.doc(`users/${event.params.userId}`);
+    // const setUserSaving = firestore.doc(`users/${event.params.userId}`).set(data, { merge: true })
+    const setUserSaving = firestore.runTransaction(t => {
+      return t.get(userRef)
+        .then(doc => {
+          let savingsArr = doc.data().savings;
+          if(!savingsArr){
+            savingsArr = [];
+          }
+          savingsArr.push(event.params.savingId);
+          t.set(userRef, { savings: savingsArr }, { merge: true });
+        })
+    })
+    const setCount = firestore.runTransaction(t => {
+      return t.get(savingRef)
+        .then(doc => {
+          const newCount = doc.data().userCount + 1;
+          t.update(savingRef, { userCount: newCount });
+        })
+    })
+    return Promise.all([setUserSaving, setCount]);
   });
